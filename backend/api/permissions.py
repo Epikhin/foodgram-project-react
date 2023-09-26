dece263db0cf3acf0b0ffec46838b92a6e47690f
Epikhin <114134984+Epikhin@users.noreply.github.com>
2023-09-26 23:22:00 +0700

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -8,7 +8,6 @@ class IsAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
-                or request.user.is_authenticated
                 and obj.author == request.user)
 
 
